75e1b718b649b280a9919013f542e88789b346f2
Fernando Rosa <fernandorosajr@gmail.com>
2019-07-15 21:33:05 -0300

    Instalação do Lokijs. Conexão ao banco de dados.

---
+++ renderer.js
@@ -0,0 +1,12 @@
+ var loki = require ('./node_modules/lokijs')
+// import loki from 'lokijs/build/lokijs.min.js';
+// imported.src = 'lokijs/build/lokijs.min.js';
+var db = new loki('db.json')
+var clientes = db.addCollection('clientes')
+
+clientes.insert({
+    nome:'fernando',
+    email: 'fernandorosajr@gmail.com'
+});
+
+db.save()
