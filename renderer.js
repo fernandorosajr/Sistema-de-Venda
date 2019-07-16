47452d4e76237ee7f77f4d88a955268386e76686
Fernando Rosa <fernandorosajr@gmail.com>
2019-07-16 14:15:49 -0300

    Teste

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
  var loki = require ('./node_modules/lokijs')
 // import loki from 'lokijs/build/lokijs.min.js';
 // imported.src = 'lokijs/build/lokijs.min.js';
+
 var db = new loki('db.json')
 var clientes = db.addCollection('clientes')
 
