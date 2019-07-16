090be38276ed062d1237cea1a3f3051e44a17aef
Fernando Rosa <fernandorosajr@gmail.com>
2019-07-16 15:30:45 -0300

    Funcionalidade da tela de cadastro de cliente: Salvamento e Reset

--- renderer.js
+++ renderer.js
@@ -7,5 +7,28 @@ clientes.insert({
     nome:'fernando',
     email: 'fernandorosajr@gmail.com'
 });
-
 db.save()
+
+function ready(fn){
+    if (document.readyState != 'loading'){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function () {
+    document.querySelector('#salvar').addEventListener('click', function(e){
+        e.preventDefault();
+        let data = {
+            nome: document.querySelector('#nome').value,
+            cpf: document.querySelector('#cpf').value,
+            telefone: document.querySelector('#telefone').value,
+          
+        };
+        clientes.insert(data);
+        db.save();
+        alert('Salvo com sucesso!');
+        document.querySelector('#cadastro-cliente').reset();
+    })
+})
